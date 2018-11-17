af0cdeb15a804a29f0ab4c2194b276ff82c501e0
aybatova <evaybatova@gmail.com>
2018-11-17 18:18:04 +0300

    The project was created and tested by auto tests

---
+++ src/main/java/ru/itpark/Sportmaster.java
@@ -0,0 +1,28 @@
+package ru.itpark;
+
+public class Sportmaster {
+  public int calculateBonus(int purchaseAmount, int totalAmount) {
+    int result;
+    int purchaseMin = 1000;
+    int bonusBlue = 50;
+    int bonusSilver = 70;
+    int bonusGold = 100;
+    int minLimitBlue = 0;
+    int maxLimitBlue = 15_000;
+    int minLimitSilver = 15_001;
+    int maxLimitSilver = 150_000;
+    int boundsCount = purchaseAmount / purchaseMin;
+
+
+    if (totalAmount >= minLimitBlue && totalAmount <= maxLimitBlue) {
+      result = ((boundsCount)) * bonusBlue;
+    } else if (totalAmount >= minLimitSilver && totalAmount <= maxLimitSilver) {
+      result = ((boundsCount)) * bonusSilver;
+    } else {
+      result = ((boundsCount)) * bonusGold;
+    }
+
+    return result;
+  }
+
+}
---
+++ src/test/java/ru/itpark/SportmasterTest.java
@@ -0,0 +1,70 @@
+package ru.itpark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SportmasterTest {
+
+    @Test
+    void calculateBonus() {
+
+        {
+            Sportmaster service = new Sportmaster();
+
+            int bonus = service.calculateBonus(100, 1_000);
+
+            assertEquals(0, bonus);
+
+        }
+        {
+            Sportmaster service = new Sportmaster();
+
+            int bonus = service.calculateBonus(1000, 14_000);
+
+            assertEquals(50, bonus);
+
+        }
+        {
+            Sportmaster service = new Sportmaster();
+
+            int bonus = service.calculateBonus(1, 15_001);
+
+            assertEquals(0, bonus);
+
+        }
+        {
+            Sportmaster service = new Sportmaster();
+
+            int bonus = service.calculateBonus(1000, 15_001);
+
+            assertEquals(70, bonus);
+
+        }
+        {
+            Sportmaster service = new Sportmaster();
+
+            int bonus = service.calculateBonus(100, 150_000);
+
+            assertEquals(0, bonus);
+
+        }
+        {
+            Sportmaster service = new Sportmaster();
+
+            int bonus = service.calculateBonus(1000, 150_001);
+
+            assertEquals(100, bonus);
+
+        }
+        {
+
+            Sportmaster service = new Sportmaster();
+
+            int bonus = service.calculateBonus(1000, 0);
+
+            assertEquals(50, bonus);
+        }
+
+    }
+}
